ba9f37c1c3d3158d59422d39b0a674f5b346e357
tan <tan@localhost.localdomain>
2015-10-22 19:38:48 -0400

    1111

---
+++ jenkins/src/main/java/rave/jenkins1/HelloWorld.java
@@ -0,0 +1,11 @@
+package rave.jenkins1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello workd from rave.jenkins1!");
+
+	}
+
+}
