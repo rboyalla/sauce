afbf99f975625369eef2e8261e87126b0aba00a8
tan <tan@localhost.localdomain>
2015-10-23 23:36:29 -0400

    22

--- jenkins/src/main/java/rave/jenkins1/HelloWorld.java
+++ jenkins/src/main/java/rave/jenkins1/HelloWorld.java
@@ -19,6 +19,8 @@ public class HelloWorld {
 		System.out.println("this is git2");
 		System.out.println("3 this is git2");
 		
+		System.out.println("added by master branch");
+		System.out.println("added by master branch22");
 	}
 
 }
